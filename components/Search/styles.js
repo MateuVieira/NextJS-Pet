990de0508678294fcfe54e07b6f6854318d84493
Mateus Vieira <mateus17v@gmail.com>
2020-11-16 20:04:04 -0300

    Add - Search component

---
+++ components/Search/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 40%;
+  height: 52px;
+  margin: 68px auto 0;
+  border-radius: 24px;
+  background-color: #999;
+`;
