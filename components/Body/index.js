10c995209b58f9bce339209f5529c82aad8d7ef6
Mateus Vieira <mateus17v@gmail.com>
2020-11-16 20:03:37 -0300

    Update - Body component

---
+++ components/Body/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Search from '../Search';
+
+import { Container } from './styles';
+
+function Body() {
+  return (
+  <Container>
+    <Search />
+  </Container>
+  );
+}
+
+export default Body;
---
+++ components/Body/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: flex;
+  width: 100%;
+  height: calc(100vh - 52px);
+  background-color: #DDD;
+  margin: 0 auto;
+`;
