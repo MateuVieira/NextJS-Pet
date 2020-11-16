b784d4489648b485906d79c0be06312c8f1dfa76
Mateus Vieira <mateus17v@gmail.com>
2020-11-16 20:02:36 -0300

    Update - import components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Header from '../components/Header';
+import Body from '../components/Body';
+
+import GlobalStyles from '../styles/reset';
 // import { Container } from './styles';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default Home;
