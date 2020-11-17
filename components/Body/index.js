7bf9248fb23d9337889c8e17adb9bd61702a1fe1
Mateus Vieira <mateus17v@gmail.com>
2020-11-16 23:22:32 -0300

    Update - import Item component and create data to render Item

--- components/Body/index.js
+++ components/Body/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState} from "react";
 
-import Search from '../Search';
+import Search from "../Search";
+import Item from "../Item";
 
-import { Container } from './styles';
+import { Container, ItemList } from "./styles";
+
+// import Vet1 from '../../asset/image/vet1.jpg'
+// import Vet2 from '../../asset/image/vet2.jpg'
+// import Loja1 from '../../asset/image/loja1.jpg'
+// import Loja2 from '../../asset/image/loja2.jpg'
+// import Loja3 from '../../asset/image/loja3.png'
 
 function Body() {
+  const [dataItens, setDataItens] = useState([
+    {
+      "id": 1,
+      "name": 'Loja',
+      "type": 'Petshop',
+      "avatarPath": 'https://petshopcontrol.blob.core.windows.net/blog/blog/wp-content/uploads/Como-ter-uma-gestao-de-estoque-incrivel-na-minha-pet-shop.jpg',
+      "location": {
+        "street": 'Rua Alguma coisa',
+        "neighborhood": 'Pq. 3 Marias',
+        "number": 'xxxx',
+      }
+    }
+  ])
+
   return (
-  <Container>
-    <Search />
-  </Container>
+    <Container>
+      <Search />
+      <ItemList>
+        <Item data={dataItens[0]} />
+        <Item data={dataItens[0]} />
+        <Item data={dataItens[0]} />
+        <Item data={dataItens[0]} />
+        <Item data={dataItens[0]} />
+        <Item data={dataItens[0]} />
+      </ItemList>
+    </Container>
   );
 }
 
--- components/Body/styles.js
+++ components/Body/styles.js
@@ -2,8 +2,21 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
+  flex-direction: column;
   width: 100%;
-  height: calc(100vh - 52px);
+  height: auto;
   background-color: #DDD;
   margin: 0 auto;
+  /* justify-content: center; */
+  align-items: center;
+`;
+
+export const ItemList = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 36px auto;
 `;
