c4df8058134c9b88f9447f343b735c74bd4a7e6a
Mateus Vieira <mateus17v@gmail.com>
2020-11-18 22:11:19 -0300

    Update - refactor component to render rationg

--- components/Item/index.js
+++ components/Item/index.js
@@ -6,9 +6,16 @@ import {
   Name, 
   Info, 
   TypeShop, 
+  Rating,
+  StarComponent,
+  StarOffComponent,
   Location, 
   Street, 
   Neighborhood,
+  ListItens,
+  VetComponent,
+  FoodComponent,
+  TosaComponent
 } from './styles';
 
 function Item(dataItem) {
@@ -18,11 +25,19 @@ function Item(dataItem) {
       <Info>
         <Name>{dataItem.data.name}</Name>
         <TypeShop>@{dataItem.data.type}</TypeShop>
+        <Rating>
+          <StarComponent />
+          <StarComponent />
+          <StarComponent />
+          <StarComponent />
+          <StarOffComponent />
+        </Rating>
+        <Location>
+          <Street>{dataItem.data.location.street}, {dataItem.data.location.number}</Street>
+          <Neighborhood>{dataItem.data.location.neighborhood}</Neighborhood>
+        </Location>
       </Info>
-      <Location>
-        <Street>{dataItem.data.location.street}, {dataItem.data.location.number}</Street>
-        <Neighborhood>{dataItem.data.location.neighborhood}</Neighborhood>
-      </Location>
+      
     </Container>
   );
 }
--- components/Item/styles.js
+++ components/Item/styles.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import {AiFillStar, AiOutlineStar} from 'react-icons/ai';
+
+import Food from '../../asset/image/food.svg'
+import Vet from '../../asset/image/vet.svg'
+import Tosa from '../../asset/image/tosa.svg'
 
 export const Container = styled.div`
   width: 80%;
@@ -10,7 +15,6 @@ export const Container = styled.div`
 
   display: flex;
   flex-direction: row;
-  /* align-items: center; */
 `;
 
 export const Avatar = styled.img`
@@ -25,14 +29,12 @@ export const Info = styled.div`
   flex-direction: column;
   margin-left: 24px;
   padding: 8px 0;
-  justify-content: center;
+  justify-content: space-around;
 `;
 
 export const Name = styled.h3`
   font-size: 26px;
   font-weight: 600;
-
-  margin-bottom: 18px;
 `;
 
 export const TypeShop = styled.h3`
@@ -41,16 +43,30 @@ export const TypeShop = styled.h3`
   color: #AAA;
 `;
 
+export const Rating = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const StarComponent = styled(AiFillStar)`
+  width: 28px;
+  height: 28px;
+  color: gold;
+`;
+
+export const StarOffComponent = styled(AiOutlineStar)`
+  width: 28px;
+  height: 28px;
+  color: gold;
+`;
+
 export const Location = styled.div`
   display: flex;
-  /* flex: 1; */
   width: 100%;
   flex-direction: column;
 
-  justify-content: center;
-  align-items: flex-end;
-
-  padding: 8px 18px;
+  justify-content: start;
+  align-items: flex-start;
 `;
 
 export const Street = styled.h3`
@@ -65,4 +81,25 @@ export const Neighborhood = styled.h3`
   font-weight: 600;
 
   color: #333;
+`;
+
+export const ListItens = styled.div`
+  display: flex;
+  flex: 1;
+  background-color: white;
+`;
+
+export const VetComponent = styled(Vet)`
+  width: 30%;
+  height: 30%;
+`;
+
+export const FoodComponent = styled(Food)`
+  width: 30%;
+  height: 30%;
+`;
+
+export const TosaComponent = styled(Tosa)`
+  width: 30%;
+  height: 30%;
 `;
